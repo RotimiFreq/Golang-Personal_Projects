d9de1c1f2bed6258e16bdac562ae782ead5bf40b
RotimiFreq <oluwarotimihz@gmail.com>
2022-07-19 19:00:42 +0100

    connecting to postgres local server

--- main.go
+++ main.go
@@ -1,15 +1,38 @@
 package main
 
-import "os"
+import (
+	"database/sql"
+	"log"
+)
+
+const (
+	db_user     = "postgres"
+	db_password = "ethanol"
+	db_name     = "test"
+)
+
+func checkErr(err error) {
+	if err != nil {
+		log.Printf("the error is :", err)
+
+	}
+
+}
+func initDB() (*sql.DB, error) {
+	db, err := sql.Open("postgres", "postgres://samuel:sam123@localhost/soundAPI_DB?sslmode=disable")
+	checkErr(err)
+
+	stmt, err := db.Prepare("CREATE TABLE PRODUCTINFO(ID SERIAL PRIMARY KEY, NAME TEXT NOT NULL);")
+
+	res, err := stmt.Exec()
+
+	log.Println(res)
+	checkErr(err)
+
+	return db, nil
+
+}
 
 func main() {
-	a := App{}
-
-	a.Initialize(
-		os.Getenv("DB_USERNAME"),
-		os.Getenv("DB_PASSWORD"),
-		os.Getenv("DB_NAME"))
-
-	a.Run(":8010")
-
+	initDB()
 }
